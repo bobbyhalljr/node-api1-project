366514c93c11e9b3e74576161dc2910494bdc1e3
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-09-30 16:12:24 -0400

    started the project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// implement your API here
+const express = require('express');
+const dataBase = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello from index.js')
+})
+
+const port = 8000;
+server.listen(port, () => console.log('API is running on port 8000'))
